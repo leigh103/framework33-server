94264abff7c35cee4f0cd1b3e9201e482799c86a
leigh103 <lee@reformedreality.com>
2020-07-15 10:23:38 +0000

    Calc Totals

--- components/transactions/models/Cart.js
+++ components/transactions/models/Cart.js
@@ -141,9 +141,11 @@
 
                 } else {
                     item.quantity = 1
+                    item.type = item_data.type
                     this.data.items.push(item)
                 }
 
+                await this.calcTotals()
                 await this.save()
 
                 return this.data
@@ -171,7 +173,9 @@
                 }
             }
 
+            await this.calcTotals()
             await this.save()
+
             return this.data
 
         }
@@ -179,6 +183,8 @@
         async empty(){
 
             this.data.items = []
+            
+            await this.calcTotals()
             await this.save()
             return this.data
 
@@ -193,6 +199,283 @@
                 return v.toString(16);
             })
             return this
+
+        }
+
+
+    // calculations
+
+
+        async calcTotals() {
+
+            this.data.sub_total = 0
+            this.data.item_total = 0
+            this.data.tax = 0
+            this.data.total = 0
+
+            await this.calcItems()
+            this.data.item_total = parseFloat(this.data.total).toFixed(2)
+            await this.calcDiscounts()
+            await this.calcTax()
+
+            if (!this.data.total){
+                this.data.item_total = "0.00"
+                this.data.sub_total = "0.00"
+                this.data.tax = "0.00"
+                this.data.total = "0.00"
+            }
+
+            return this
+
+        }
+
+        async calcItems() {
+
+            this.data.sub_total = 0
+
+            let i = 0;
+
+            if (!this.data.items || this.data.items.length <= 0){
+
+                this.data.items = []
+                this.data.total = 0
+
+                return this
+
+            } else {
+
+                for (let item of this.data.items) {
+
+                    if (item.type == 'vouchers' && item.value){ // if a voucher, turn the value negative
+
+                        item.price = item.value
+                        item.name = 'Voucher £'+item.value
+
+                    }
+
+                    if (item.price){
+
+                        if (item.adjustment){
+
+                            if (item.original_price){
+                                item.price = item.original_price
+                            }
+
+                            item.adjustment = item.adjustment.replace(/\$|\£|\#|p/,'')
+
+                            if (item.adjustment.match(/%/)){
+
+                                item.adjustment_value = item.adjustment.replace(/%/,'')
+                                item.adjustment_value = (item.price/100)*item.adjustment_value
+                                item.original_price = parseFloat(item.price).toFixed(2)
+                                item.price = parseFloat(item.price)+parseFloat(item.adjustment_value)
+
+                            } else {
+
+                                item.original_price = parseFloat(item.price).toFixed(2)
+                                item.price = parseFloat(item.price)+parseFloat(item.adjustment)
+
+                            }
+
+                        }
+
+                        item.price = parseFloat(item.price).toFixed(2)
+
+                        if (item.quantity>1){
+                            this.data.total = parseFloat(this.data.total)+(parseFloat(item.price)*parseInt(item.quantity))
+                        } else {
+                            item.quantity = 1
+                            this.data.total = parseFloat(this.data.total)+parseFloat(item.price)
+                        }
+
+                        if (item.type != 'vouchers' && item.type != 'account'){
+                            item.sub_total = parseFloat(item.price)/config.tax_amount
+                            item.tax = parseFloat(item.price)-parseFloat(item.sub_total)
+                            item.tax = (item.tax*parseInt(item.quantity)).toFixed(2)
+                            item.sub_total = (item.sub_total*parseInt(item.quantity)).toFixed(2)
+                        }
+
+                    }
+
+                }
+
+                return this
+
+            }
+
+        }
+
+        async calcTax() {
+
+            for (let item of this.data.items) {
+
+                if (item.price){
+                //    cart.total += parseFloat(item.price)
+                }
+                if (item.tax){
+                    this.data.tax += parseFloat(item.tax)
+                }
+                if (item.sub_total){
+                    this.data.sub_total += parseFloat(item.sub_total)
+                }
+
+            }
+
+            this.data.sub_total = parseFloat(this.data.sub_total).toFixed(2)
+            this.data.tax = parseFloat(this.data.tax).toFixed(2)
+            this.data.total = parseFloat(this.data.total).toFixed(2)
+
+            return this
+
+        }
+
+        async calcDiscounts(){
+
+            console.log('calcDiscounts')
+
+            await this.applyOfferCode()
+            await this.applyVouchers()
+            await this.applyAccountBalance()
+
+            return this
+
+            // return new Promise(async function(resolve, reject){
+            //
+            //     if (typeof cart == 'object'){
+            //
+            //         cart = await transactions.applyOfferCode(cart)
+            //         cart = await transactions.applyVouchers(cart)
+            //         cart = await transactions.applyAccountBalance(cart)
+            //
+            //         resolve(cart)
+            //
+            //     } else {
+            //
+            //         reject('not found')
+            //
+            //     }
+            //
+            // })
+
+        }
+
+        async applyOfferCode() {
+
+            console.log('applyOffer')
+            return this
+
+            // return new Promise(async (resolve, reject) => {
+            //
+            //     if (cart.offer_code_data && cart.offer_code_data.method && cart.offer_code_data.value){
+            //
+            //         let item_discount
+            //
+            //         cart.payment.discount = 0
+            //
+            //         for (let item of cart.items){
+            //
+            //             if (!item.original_price){
+            //                 item.original_price = item.price
+            //             }
+            //
+            //             if (cart.offer_code_data.method == 'percent_off'){
+            //                 item_discount = item.original_price * (parseFloat(cart.offer_code_data.value)/100)
+            //             } else if (cart.offer_code_data.method == 'fixed_off'){
+            //                 item_discount = item.original_price - parseFloat(cart.offer_code_data.value)
+            //             }
+            //
+            //             cart.payment.discount = cart.payment.discount+item_discount
+            //
+            //             item.price = parseFloat(item.original_price) - item_discount
+            //             item.price = item.price.toFixed(2)
+            //
+            //         }
+            //
+            //         resolve(cart)
+            //
+            //     } else {
+            //
+            //         cart.payment.discount = 0
+            //
+            //         for (let item of cart.items){
+            //
+            //             if (item.original_price){
+            //                 item.price = item.original_price
+            //             }
+            //             if (item.price){
+            //                 item.price = parseFloat(item.price).toFixed(2)
+            //             }
+            //
+            //         }
+            //
+            //         resolve(cart)
+            //     }
+            //
+            // })
+
+        }
+
+        async applyVouchers() {
+
+            console.log('applyVouchers')
+            return this
+
+            // return new Promise(function(resolve, reject) {
+            //
+            //     if (cart.vouchers && cart.vouchers.length > 0){
+            //
+            //         cart.payment.vouchers = cart.vouchers.reduce((total,a)=>{
+            //             return total + parseFloat(a.value)
+            //         },0)
+            //
+            //         cart.vouchers_total = cart.payment.vouchers
+            //         cart.total = cart.total - cart.payment.vouchers
+            //
+            //         if (cart.total <= 0){
+            //             let diff = Math.abs(cart.total) * 1
+            //             cart.total = 0
+            //             cart.payment.vouchers = cart.payment.vouchers - diff
+            //             cart.vouchers_total = cart.payment.vouchers
+            //         }
+            //
+            //         resolve(cart)
+            //
+            //     } else {
+            //         cart.payment.vouchers = 0
+            //         resolve(cart)
+            //     }
+            //
+            // })
+
+        }
+
+        async applyAccountBalance() {
+
+            console.log('applyAccountBalance')
+            return this
+
+            // return new Promise(function(resolve, reject) {
+            //
+            //     if (cart.account_balance){
+            //
+            //         cart.payment.account = parseFloat(cart.account_balance)
+            //
+            //         cart.total = cart.total - cart.payment.account
+            //
+            //         if (cart.total <= 0){
+            //             let diff = Math.abs(cart.total) * 1
+            //             cart.total = 0
+            //             cart.payment.account = cart.payment.account - diff
+            //         }
+            //         cart.account_total = cart.payment.account
+            //         resolve(cart)
+            //
+            //     } else {
+            //         cart.payment.account = 0
+            //         resolve(cart)
+            //     }
+            //
+            // })
 
         }
 
